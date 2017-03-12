cb349db0b5551809b061cf175dfe40c109f315d6
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-03-12 23:14:23 +0700

    Initial commit

---
+++ do_build_petsc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+INC="-I/home/efefer/mysoftwares/petsc-3.7.5/include/"
+LIB="-L/home/efefer/mysoftwares/petsc-3.7.5/lib -lpetsc"
+
+bas=`basename $1 .F`
+
+mpifort -ffree-form $INC $1 $LIB -o $bas.x
+
